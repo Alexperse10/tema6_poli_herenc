50f966e9ae496390111a21aeabc7c32265fdbfbd
AlexPerse10 <spuknitfire@gmail.com>
2026-03-03 19:20:02 +0100

    se realizan ejercicios de book

--- ejercicios_book/src/main/java/org/example/Bombilla.java
+++ ejercicios_book/src/main/java/org/example/Bombilla.java
@@ -1,5 +1,43 @@
 package org.example;
 
 public class Bombilla {
-    
+    private boolean estado;
+
+    public Bombilla(){
+        this.estado = false;
+    }
+
+    public void encender(){
+        if (!estado) {  // si la bombilla no esta encendida es decir si no es true
+            estado = true;
+            System.out.println("la bombilla esta encendida");
+        }
+    }
+
+    public void apagar() {
+        if (estado) {
+            estado = false;
+            System.out.println("la bombilla esta apagada");
+        }
+    }
+
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void mostrarinfo() {
+        System.out.println("el estado de la bombilla es: "+estado);
+    }
+
+    @Override
+    public String toString() {
+        return "Bombilla{" +
+                "estado=" + estado +
+                '}';
+    }
 }
---
+++ ejercicios_book/src/main/java/org/example/BombillaApp.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class BombillaApp {
+    public static void main(String[] args) {
+        Bombilla bombilla1 = new Bombilla();
+        bombilla1.encender();
+        bombilla1.mostrarinfo();
+        bombilla1.apagar();
+        bombilla1.mostrarinfo();
+    }
+}
